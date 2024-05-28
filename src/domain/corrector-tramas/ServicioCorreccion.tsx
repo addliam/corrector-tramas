c2c1cdfacd4ecc7592f124e9e17d4c48ecd9dfd1
codeli4m <liamyostin2004@gmail.com>
2024-05-28 15:39:21 -0500

    quitaron comentario innecesario

--- src/domain/corrector-tramas/ServicioCorreccion.tsx
+++ src/domain/corrector-tramas/ServicioCorreccion.tsx
@@ -2,9 +2,7 @@ import { Corrector } from "./interface/Corrector";
 import { IParser } from "./interface/IParser";
 
 export class ServicioCorreccion {
-  // no inyectar dependencia de Fabrica ya que siempre sera la misma fabrica de N algoritmos
   private algoritmoCorreccion: Corrector & IParser;
-  // separamos responsabilidad de: correccion y parseo
   constructor(algoritmoCorreccion: Corrector & IParser) {
     this.algoritmoCorreccion = algoritmoCorreccion;
   }
